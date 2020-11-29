bbf08a8e4ff06ee6e947f7f7c59dca473352adbd
Rui LIU <xxliuruiabc@gmail.com>
2020-11-29 20:44:41 +0800

    + doc, rename vars

--- AUROC.hpp
+++ AUROC.hpp
@@ -25,22 +25,27 @@
 
 #include <algorithm>
 
+/// @tparam T Type of array elements, should be a floating number type
+/// @param yTrue Array of ground truth labels, 0.0 is negative, 1.0 is positive
+/// @param yPred Array of predicted scores, can be of any range
+/// @param n Number of elements in the array
+/// @return AUROC/ROC-AUC score, range [0.0, 1.0]
 template<class T>
-double AUROC(const T* y_true, const T* y_pred, size_t n) {
+double AUROC(const T* yTrue, const T* yPred, size_t n) {
 	for (size_t i = 0; i < n; i++) {
-		if (std::isnan(y_pred[i]) || std::isinf(y_pred[i]))
+		if (std::isnan(yPred[i]) || std::isinf(yPred[i]))
 			return -1;
 	}
 
 	const auto index = new size_t[n];
 	for (size_t i = 0; i < n; i++) index[i] = i;
 	// std::iota(index, index + n, 0); // Equivalent to above
-	std::sort(index, index + n, [&](size_t a, size_t b) { return y_pred[a] > y_pred[b]; });
+	std::sort(index, index + n, [&](size_t a, size_t b) { return yPred[a] > yPred[b]; });
 	const auto y = new double[n]; // Desc
 	const auto z = new double[n]; // Desc
 	for (size_t i = 0; i < n; i++) {
-		y[i] = y_true[index[i]];
-		z[i] = y_pred[index[i]];
+		y[i] = yTrue[index[i]];
+		z[i] = yPred[index[i]];
 	}
 
 	const auto tp = y; // Reuse
@@ -66,17 +71,17 @@ double AUROC(const T* y_true, const T* y_pred, size_t n) {
 	}
 	delete[] index;
 
-	const auto tps_diff = new double[n];
-	const auto fps_diff = new double[n];
+	const auto tp_diff = new double[n];
+	const auto fp_diff = new double[n];
 	for (size_t i = 1; i < n; i++) {
-		tps_diff[i] = tp[i] - tp[i - 1];
-		fps_diff[i] = fp[i] - fp[i - 1];
+		tp_diff[i] = tp[i] - tp[i - 1];
+		fp_diff[i] = fp[i] - fp[i - 1];
 	} // Equivalent to below
-	// std::adjacent_difference(tp, tp + n, tps_diff);
-	// std::adjacent_difference(fp, fp + n, fps_diff);
+	// std::adjacent_difference(tp, tp + n, tp_diff);
+	// std::adjacent_difference(fp, fp + n, fp_diff);
 	top = 1;
 	for (size_t i = 1; i < n - 1; i++) {
-		if (tps_diff[i] != tps_diff[i + 1] || fps_diff[i] != fps_diff[i + 1]) {
+		if (tp_diff[i] != tp_diff[i + 1] || fp_diff[i] != fp_diff[i + 1]) {
 			tp[top] = tp[i];
 			fp[top] = fp[i];
 			top++;
@@ -85,8 +90,8 @@ double AUROC(const T* y_true, const T* y_pred, size_t n) {
 	tp[top] = tp[n - 1];
 	fp[top] = fp[n - 1];
 	n = ++top; // Size of tp/fp -> size of optimized tp/fp 
-	delete[] tps_diff;
-	delete[] fps_diff;
+	delete[] tp_diff;
+	delete[] fp_diff;
 
 	for (size_t i = 0; i < n; i++) {
 		tp[i] /= tp[n - 1];
