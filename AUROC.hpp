02a73d1d47626558467b888261139e36286bd832
Rui LIU <xxliuruiabc@gmail.com>
2021-04-15 15:09:03 +0800

    Use STL

--- AUROC.hpp
+++ AUROC.hpp
@@ -24,23 +24,22 @@
 #pragma once
 
 #include <cmath>
+#include <numeric>
 #include <algorithm>
 
 /// @tparam T Type of array elements, should be a floating number type
-/// @param yTrue Array of ground truth labels, 0.0 is negative, 1.0 is positive
+/// @param yTrue Array of ground truth labels, 0 is negative, 1 is positive
 /// @param yPred Array of predicted scores, can be of any range
 /// @param n Number of elements in the array
 /// @return AUROC/ROC-AUC score, range [0.0, 1.0]
 template<class T>
 double AUROC(const T* yTrue, const T* yPred, size_t n) {
-	for (size_t i = 0; i < n; i++) {
+	for (size_t i = 0; i < n; i++)
 		if (std::isnan(yPred[i]) || std::isinf(yPred[i]) || yTrue[i] != 0 && yTrue[i] != 1)
 			return std::numeric_limits<double>::quiet_NaN();
-	}
 
 	const auto index = new size_t[n];
-	for (size_t i = 0; i < n; i++) index[i] = i;
-	// std::iota(index, index + n, 0); // Equivalent to above
+	std::iota(index, index + n, 0);
 	std::sort(index, index + n, [&](size_t a, size_t b) { return yPred[a] > yPred[b]; });
 	const auto y = new double[n]; // Desc
 	const auto z = new double[n]; // Desc
@@ -50,17 +49,12 @@ double AUROC(const T* yTrue, const T* yPred, size_t n) {
 	}
 
 	const auto tp = y; // Reuse
-	tp[0] = y[0];
-	for (size_t i = 1; i < n; i++) {
-		tp[i] = tp[i - 1] + y[i];
-	} // Equivalent to below
-	// std::partial_sum(y, y + n, tp);
+	std::partial_sum(y, y + n, tp);
 
 	size_t top = 0; // # diff
-	for (size_t i = 0; i < n - 1; i++) {
+	for (size_t i = 0; i < n - 1; i++)
 		if (z[i] != z[i + 1])
 			index[top++] = i;
-	}
 	index[top++] = n - 1;
 	n = top; // Size of y/z -> sizeof tps/fps
 	delete[] z;
@@ -74,20 +68,15 @@ double AUROC(const T* yTrue, const T* yPred, size_t n) {
 
 	const auto tp_diff = new double[n];
 	const auto fp_diff = new double[n];
-	for (size_t i = 1; i < n; i++) {
-		tp_diff[i] = tp[i] - tp[i - 1];
-		fp_diff[i] = fp[i] - fp[i - 1];
-	} // Equivalent to below
-	// std::adjacent_difference(tp, tp + n, tp_diff);
-	// std::adjacent_difference(fp, fp + n, fp_diff);
+	std::adjacent_difference(tp, tp + n, tp_diff);
+	std::adjacent_difference(fp, fp + n, fp_diff);
 	top = 1;
-	for (size_t i = 1; i < n - 1; i++) {
+	for (size_t i = 1; i < n - 1; i++)
 		if (tp_diff[i] != tp_diff[i + 1] || fp_diff[i] != fp_diff[i + 1]) {
 			tp[top] = tp[i];
 			fp[top] = fp[i];
 			top++;
 		}
-	}
 	tp[top] = tp[n - 1];
 	fp[top] = fp[n - 1];
 	n = ++top; // Size of tp/fp -> size of optimized tp/fp 
@@ -100,10 +89,8 @@ double AUROC(const T* yTrue, const T* yPred, size_t n) {
 	}
 
 	double area = tp[0] * fp[0] / 2; // The first triangle from origin
-	for (size_t i = 0; i < n - 1; i++) {
+	for (size_t i = 0; i < n - 1; i++)
 		area += (tp[i] + tp[i + 1]) * (fp[i + 1] - fp[i]) / 2;
-	}
-
 	delete[] tp;
 	delete[] fp;
 
